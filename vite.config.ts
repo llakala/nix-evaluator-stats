33ac91e33982c59f84a6dbe80b71d558797f8331
NotAShelf <raf@notashelf.dev>
2026-01-22 22:50:47 +0300

    ci: build & deploy to GH pages
    Signed-off-by: NotAShelf <raf@notashelf.dev>
    Change-Id: Ieedfca23ce8b2b52ebae5b81095973056a6a6964

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import solidPlugin from 'vite-plugin-solid';
 
 export default defineConfig({
   plugins: [solidPlugin()],
+  base: process.env.GITHUB_PAGES ? '/nix-evaluator-stats/' : './',
   server: {
     port: 3000,
     open: false,
